8d86aff7e4a195a2ab5cb04badd245546efb1adc
Niranjana009 <electrons981@gmail.com>
2022-06-16 10:00:52 +0530

    protected assignment

---
+++ Core_Java/src/com/xworkz/core_java/Assignments/access_modifiers/protected0/Bike.java
@@ -0,0 +1,5 @@
+package com.xworkz.core_java.Assignments.access_modifiers.protected0;
+
+public class Bike {
+	protected String name = "Honda";
+}
---
+++ Core_Java/src/com/xworkz/core_java/Assignments/access_modifiers/protected1/Honda.java
@@ -0,0 +1,14 @@
+package com.xworkz.core_java.Assignments.access_modifiers.protected1;
+
+import com.xworkz.core_java.Assignments.access_modifiers.protected0.Bike;
+
+public class Honda extends Bike{
+	
+		
+		public static void main(String[] args) {
+			Honda honda = new Honda();
+			System.out.println("BIke name is:"+ honda.name);
+		}
+
+	}
+
