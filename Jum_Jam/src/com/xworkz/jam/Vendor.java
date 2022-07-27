a45a9c6207b58702b523fe2c31fef7f0edf81a50
Niranjana009 <electrons981@gmail.com>
2022-07-27 23:06:54 +0530

    Jum_Jam Jar file

---
+++ Jum_Jam/src/com/xworkz/jam/Vendor.java
@@ -0,0 +1,13 @@
+package com.xworkz.jam;
+
+public class Vendor {
+		
+		public Vendor(){
+			System.out.println("I am Jim Jam Vendor");
+		}
+		
+		public String getname() {
+			return "Jum JAm";
+		}
+
+}
