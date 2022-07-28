447265d2662ad5dbbc3fca787df0ec6d71be0b6e
Niranjana009 <electrons981@gmail.com>
2022-07-28 16:44:41 +0530

    "JDBC"

--- MySqlInception/src/com/xworkz/MySql/JdbcDBConnect.java
+++ MySqlInception/src/com/xworkz/MySql/JdbcDBConnect.java
@@ -8,7 +8,7 @@ public class JdbcDBConnect {
 
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
 		Class.forName("com.mysql.cj.jdbc.Driver");
-		Connection connection = DriverManager.getConnection("jdbc:mysql://Localhost:3306","root","Mysql@981");
+		Connection connection = DriverManager.getConnection("jdbc:mysql://Localhost:3306","root","Mysql@981");// connection
 		System.out.println(connection);
 	}
 
